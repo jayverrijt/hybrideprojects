c0bcb84c87e805913b514992ace32bb1ba341c18
Jay Verrijt <109523337+jayverrijt@users.noreply.github.com>
2023-03-03 09:40:24 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
     // PC
-    $conn = mysqli_connect('localhost', 'root', 'Woezel-2005!', 'dbstage') or die('Helaas verbinding met de database mislukt');
+    $conn = mysqli_connect('localhost', 'root', '', 'dbstage') or die('Helaas verbinding met de database mislukt');
     // Server
     // $conn = mysqli_connect('localhost', 'dbssjv', 'Woezel-2005!', 'dbss') or die('Helaas verbinding met de database mislukt');
 ?>
